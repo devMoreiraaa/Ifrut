8fe1b5bea8f51958ff2cc8461528bd7287a546fd
leonardo <leonardoazevedomoreiraa@gmail.com>
2024-04-12 16:30:11 -0300

    Initial Commit

---
+++ Ifrut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ifrut
+//
+//  Created by leonardo Moreira on 12/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ifrut/IfrutApp.swift
@@ -0,0 +1,17 @@
+//
+//  IfrutApp.swift
+//  Ifrut
+//
+//  Created by leonardo Moreira on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct IfrutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
